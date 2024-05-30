0ea66621b63c446b958448dafa15b4c3cb144976
Joel Moyano <hyperwolf4@gmail.com>
2024-05-30 18:50:15 -0300

    Se cambiaron algunas cosas del script y se añadieron comentarios

--- Ejercicio 4/script.js
+++ Ejercicio 4/script.js
@@ -1,12 +1,24 @@
-const formulario = $('formulario')
-
-formulario.addEventListener('submit', (e)=>{
-    let peso = +formulario['peso'].value
-    let altura = +formulario['altura'].value
-    alert(`El IMC es ${peso/(altura*altura)}`)
-    e.preventDefault()
-})
-
-function $(elemento){
-    return document.getElementById(elemento)
+// Función auxiliar para obtener un elemento del DOM por su id
+function $(elemento) {
+    return document.getElementById(elemento);
 }
+
+// Obtiene el formulario del DOM
+const formulario = $('formulario');
+
+// Agrega un evento de escucha para el evento de envío del formulario
+formulario.addEventListener('submit', (e) => {
+    // Obtiene los valores de peso y altura del formulario y los convierte a números
+    let peso = +formulario['peso'].value;
+    let altura = +formulario['altura'].value;
+
+    // Calcula el índice de masa corporal (IMC) usando la fórmula: peso / (altura * altura)
+    let IMC = peso / (altura * altura);
+
+    // Muestra un mensaje de alerta con el IMC calculado
+    alert(`El IMC es ${IMC}`);
+
+    // Evita que el formulario se envíe y recargue la página
+    e.preventDefault();
+});
+
